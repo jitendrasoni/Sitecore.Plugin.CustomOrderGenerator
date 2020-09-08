04d4dd490451921affe578c77c6c12286341ac40
Jitendra Soni <jitusonijk@gmail.com>
2020-09-08 12:35:30 +0530

    Initial setup for the custom plugin
    Initial setup for the custom plugin

---
+++ ConfigureSitecore.cs
@@ -0,0 +1,37 @@
+﻿// © 2019 Sitecore Corporation A/S. All rights reserved. Sitecore® is a registered trademark of Sitecore Corporation A/S.
+
+using System.Reflection;
+using Microsoft.Extensions.DependencyInjection;
+using Sitecore.Commerce.Core;
+using Sitecore.Commerce.Plugin.Orders;
+using Sitecore.Framework.Configuration;
+using Sitecore.Framework.Pipelines.Definitions.Extensions;
+
+namespace Sitecore.Commerce.Plugin.Sample
+{
+    /// <summary>
+    /// The configure sitecore class.
+    /// </summary>
+    public class ConfigureSitecore : IConfigureSitecore
+    {
+        /// <summary>
+        /// The configure services.
+        /// </summary>
+        /// <param name="services">
+        /// The services.
+        /// </param>
+        public void ConfigureServices(IServiceCollection services)
+        {
+            var assembly = Assembly.GetExecutingAssembly();
+            services.RegisterAllPipelineBlocks(assembly);
+
+            services.Sitecore().Pipelines(config => config
+
+                .ConfigurePipeline<IOrderPlacedPipeline>(builder => builder.Replace<OrderPlacedAssignConfirmationIdBlock,
+                CustomOrderGeneratorBlock>())
+            );
+
+            services.RegisterAllCommands(assembly);
+        }
+    }
+}
